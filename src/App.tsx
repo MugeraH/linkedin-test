beed268f2af01186e6c0a486ba0a32b9002fffcb
mugerah <hughes.mugera@touchinspiration.com>
2021-08-26 15:23:38 +0300

    trail one

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 // import axios from "axios";
+import { useParams } from "react-router-dom";
 
 import Nav from "react-bootstrap/Nav";
 import "./App.css";
@@ -8,8 +9,30 @@ import "./App.css";
 import logo from "./assets/linkedin.png";
 
 function App() {
+  const [state, setState] = useState(false);
   // let redirect_url = "http://localhost:3000";
   // let client_id = "774up8xbssfyp5";
+
+  var getAuthToken = function (url: string) {
+    var params = {};
+    (url + "?")
+      .split("?")[1]
+      .split("&")
+      .forEach(function (pair) {
+        //@ts-ignore
+        pair = (pair + "=").split("=").map(decodeURIComponent);
+        if (pair[0].length) {
+          //@ts-ignore
+          params[pair[0]] = pair[1];
+        }
+      });
+
+    return params;
+  };
+  let url = window.location.href;
+  let data = getAuthToken(url);
+  // @ts-ignore
+  console.log(data.code);
 
   return (
     <div className="App">
@@ -31,7 +54,7 @@ function App() {
 
           <a
             href="https://www.linkedin.com/oauth/v2/authorization?response_type=code&state=657575&scope=r_liteprofile%20r_emailaddress&client_id=774up8xbssfyp5&redirect_uri=
-https%3A%2F%2Flinkedin-test-ten.vercel.app"
+http%3A%2F%2Flocalhost%3A3000"
           >
             Linkedin
           </a>
@@ -43,6 +66,8 @@ https%3A%2F%2Flinkedin-test-ten.vercel.app"
           Linkedin
         </Link> */}
       </div>
+
+      {state ? <h2>Connected</h2> : <h2> Not Connected</h2>}
     </div>
   );
 }
