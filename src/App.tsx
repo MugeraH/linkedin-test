ea053156403c350b64a5c43b1100efbfc0bd5321
mugerah <hughes.mugera@touchinspiration.com>
2021-08-26 14:23:06 +0300

    ass linkin btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import axios from "axios";
 
-import './App.css';
+import Nav from "react-bootstrap/Nav";
+import "./App.css";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import logo from "./assets/linkedin.png";
 
 function App() {
+  let redirect_url = "http://localhost:3000";
+  let client_id = "774up8xbssfyp5";
+
   return (
     <div className="App">
-  Hi Hughes
+      <Nav className="justify-content-center" activeKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">Link</Nav.Link>
+        </Nav.Item>
+      </Nav>
+
+      <div className="container">
+        <div className="link">
+          <img src={logo} alt="linkedin logo" />
+
+          <a
+            href="https://www.linkedin.com/oauth/v2/authorization?response_type=code&state=657575&scope=r_liteprofile%20r_emailaddress&client_id=774up8xbssfyp5&redirect_uri=
+https%3A%2F%2Flinkedin-test-ten.vercel.app"
+          >
+            Linkedin
+          </a>
+        </div>
+        {/* <Link
+          className="btn btn-primary"
+          to="https://www.linkedin.com/oauth/v2/authorization?response_type=code&state=657575&scope=r_liteprofile%20r_emailaddress&client_id=774up8xbssfyp5redirect_uri=http%3A%2F%2Flocalhost%3A3000"
+        >
+          Linkedin
+        </Link> */}
+      </div>
     </div>
   );
 }
